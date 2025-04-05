4accbc4b1519733aa0016a22501bb893476da96f
justvarma <146994690+justvarma@users.noreply.github.com>
2025-04-05 19:31:10 +0530

    update

--- TicTacToe_ai/player.py
+++ TicTacToe_ai/player.py
@@ -44,3 +44,14 @@ def GeniusComputerPlayer(Player):
         else:
             square=self.minimax(game, self.letter)
         return square
+
+    def minimax(self, state, player):
+        max_player=self.letter
+        other_player='0' if player=='X' else 'X'
+
+        if state.current_winner==other_player:
+            return{'position': None, 
+                   'score': 1 * (state.num_empty_square() + 1) if other_player==max_player else -1 *(
+                       state.num_empty_squares()+1)
+            }
+        elif not state
