df6e3992324a77cbbc33b2d644199fcafbfd4e3f
justvarma <146994690+justvarma@users.noreply.github.com>
2025-04-05 19:25:02 +0530

    update

--- TicTacToe_ai/player.py
+++ TicTacToe_ai/player.py
@@ -33,3 +33,14 @@ class HumanPlayer(Player):
             except ValueError:
                 print("Invalid square. Try again.")
         return val
+
+def GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square=random.choice(game.available_moves())
+        else:
+            square=self.minimax(game, self.letter)
+        return square
